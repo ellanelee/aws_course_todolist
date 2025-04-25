b4e31dc1925e75f2063b2a3cafac638ef34b23de
ellanelee <ljedoc@naver.com>
2025-04-25 17:40:59 +0900

    2nd

--- src/app/tomorrow/page.jsx
+++ src/app/tomorrow/page.jsx
@@ -10,10 +10,10 @@ export default function TodoList() {
         alignItems: "center",
       }}
     >
-      <div style={{ marginTop: "10px" }}>아침 운동</div>
-      <div style={{ marginTop: "10px" }}>오리엔테이션 및 오전수업</div>
+      <div style={{ marginTop: "10px" }}>오전수업</div>
+      <div style={{ marginTop: "10px" }}>과제</div>
       <div style={{ marginTop: "10px" }}>점심 식사</div>
-      <div style={{ marginTop: "10px" }}>오후 수업</div>
+      <div style={{ marginTop: "10px" }}>동아리 모임</div>
     </main>
   )
 }
